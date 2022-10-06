6866e7908ea79ff4b20e80007dadf95a6e1e5237
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2022-10-06 10:50:29 -0300

    Update xxx user-agent

--- scanners/scan_xss.py
+++ scanners/scan_xss.py
@@ -7,28 +7,23 @@ import os
 import sys
 from urllib.request import urlopen, Request
 from urllib.error import URLError, HTTPError
-from colorama import Back, Fore, init
-
-init()
-
-#Stuff related to Mechanize browser module
-br = mechanize.Browser() #Shortening the call by assigning it to a varaible "br"
-# set cookies
-cookies = http.cookiejar.LWPCookieJar()
-br.set_cookiejar(cookies)
-# Mechanize settings
-br.set_handle_equiv(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-br.set_debug_http(False)
-br.set_debug_responses(False)
-br.set_debug_redirects(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
-('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'), ('Accept-Encoding','br')]
+import random
 
 
+user_agents = [
+ "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8",
+ "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre",
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)",
+ "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)",
+ "Mozilla/3.01 (Macintosh; PPC)",
+ "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",  
+ "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",  
+ "Opera/8.00 (Windows NT 5.1; U; en)",  
+ "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
+]
+
+user_agent = random.choice (user_agents)
+headers = {'User-Agent': user_agent}
 
 def xss(l,wordlist,urls_vulnerables):
     print()
