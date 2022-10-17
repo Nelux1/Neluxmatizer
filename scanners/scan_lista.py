30f0c59c9c9ae8d623f96cf70960637254dfdb48
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2022-10-17 15:12:43 -0300

    reparando lista de escanners

--- scanners/scan_lista.py
+++ scanners/scan_lista.py
@@ -30,31 +30,17 @@ br.set_debug_http(False)
 br.set_debug_responses(False)
 br.set_debug_redirects(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
-
-user_agents = [
- "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8",
- "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre",
- "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)",
- "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)",
- "Mozilla/3.01 (Macintosh; PPC)",
- "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",  
- "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",  
- "Opera/8.00 (Windows NT 5.1; U; en)",  
- "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
-]
-
-user_agent = random.choice (user_agents)
-
-br.addheaders = [(user_agent),
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
 ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'), ('Accept-Encoding','br')]
 
 def all_list(l,c,cl,h,x,lf,s,i,sr,output,fname,o,vulnerables_urls):   
      
  for linea in l:
+
      try:
          print()
          print('---------------------')
-         print('\033[1;32m' + linea+':\033[0m')
+         print('\033[1;32m' + linea+ ':\033[0m')
          print('---------------------')
          print()
          if 'http://' in linea:
@@ -70,64 +56,63 @@ def all_list(l,c,cl,h,x,lf,s,i,sr,output,fname,o,vulnerables_urls):
              linea = 'https://' + linea
              br.open(linea)    
          #forms = br.forms() #Finds all the forms present in webpage
-
+ 
          headers = str(urlopen(linea).headers).lower()
 
-        
          if h:
-            if 'strict-transport-security' not in headers:
-                print ('\033[1;32m[+]\033[0m ' + linea + ' \033[1;32mNot force HSTS\033[0m')
-                vulnerables_urls.append('****************** VULNERABLE TO HSTS: *********************')
-                vulnerables_urls.append(linea)      
-            else:
-                print ('\033[1;31m[-]\033[0m ' + linea + ' \033[1;31mHSTS is OK\033[0m')  
-         
+                if 'strict-transport-security' not in headers:
+                    print ('\033[1;32m[+]\033[0m ' + linea + ' \033[1;32mNot force HSTS\033[0m')
+                    vulnerables_urls.append('****************** VULNERABLE TO HSTS: *********************')
+                    vulnerables_urls.append(linea)      
+         else:
+                    print ('\033[1;31m[-]\033[0m ' + linea + ' \033[1;31mHSTS is OK\033[0m')  
+            
          if cl:
-            if 'x-frame-options' not in headers:
-                if 'content-security-policy' not in headers:
-                 print ('\033[1;32m[+]\033[0m ' + linea + ' \033[1;32mvulnerable to Clickjacking\033[0m')
-                 vulnerables_urls.append('****************** VULNERABLE TO CLICKJACKING: *********************')
-                 vulnerables_urls.append(linea)
-                else:
-                 print ('\033[1;31m[-]\033[0m '  + linea + ' \033[1;31mis not vulnerable to Clickjacking\033[0m')
-            else:
-                 print ('\033[1;31m[-]\033[0m '  + linea + ' \033[1;31mis not vulnerable to Clickjacking\033[0m')
-         
-         if c:
-                br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
-         ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'), ('Accept-Encoding','br'), ('Origin','https://evil.com')]
-
-                if 'access-control-allow-origin' in headers:
-                    if 'https://evil.com' in headers:
-                        print ('\033[1;32m[+]\033[0m ' + linea + ' \033[1;32mis vulnerable to Cors\033[0m')
-                        vulnerables_urls.append('****************** VULNERABLE TO CORS: *********************') 
-                        vulnerables_urls.append(linea)                           
+                if 'x-frame-options' not in headers:
+                    if 'content-security-policy' not in headers:
+                        print ('\033[1;32m[+]\033[0m ' + linea + ' \033[1;32mvulnerable to Clickjacking\033[0m')
+                        vulnerables_urls.append('****************** VULNERABLE TO CLICKJACKING: *********************')
+                        vulnerables_urls.append(linea)
                     else:
-                        print ('\033[1;31m[-]\033[0m ' + linea + ' \033[1;31mis not vulnerable to Cors\033[0m')
-                else:
-                    print ('\033[1;31m[-]\033[0m ' + linea + ' \033[1;31mis not vulnerable to Cors\033[0m')
+                        print ('\033[1;31m[-]\033[0m '  + linea + ' \033[1;31mis not vulnerable to Clickjacking\033[0m')
+         else:
+                    print ('\033[1;31m[-]\033[0m '  + linea + ' \033[1;31mis not vulnerable to Clickjacking\033[0m')
+            
+         if c:
+                    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'),
+            ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'), ('Accept-Encoding','br'), ('Origin','https://evil.com')]
+
+                    if 'access-control-allow-origin' in headers:
+                        if 'https://evil.com' in headers:
+                            print ('\033[1;32m[+]\033[0m ' + linea + ' \033[1;32mis vulnerable to Cors\033[0m')
+                            vulnerables_urls.append('****************** VULNERABLE TO CORS: *********************') 
+                            vulnerables_urls.append(linea)                           
+                        else:
+                            print ('\033[1;31m[-]\033[0m ' + linea + ' \033[1;31mis not vulnerable to Cors\033[0m')
+                    else:
+                     print ('\033[1;31m[-]\033[0m ' + linea + ' \033[1;31mis not vulnerable to Cors\033[0m')
      except:
-         continue
+         continue   
      try:    
          if x:
-            uri=[]
-            wordlist=['"><script>confirm(1)</script>']
-            try:
-                parametizer(linea,output)
-                with open(output, "r") as f:
-                    for i in f.readlines():
-                        i = i.strip()
-                        if i == "" or i.startswith("#"):
-                            continue
-                        uri.append(i)            
-                print()        
-                print('\033[1;33mTest xss for default payload:\033[0m')
-                print()        
-                xss(uri,wordlist,vulnerables_urls)           
-            except: 
+             uri=[]
+             wordlist=['"><script>confirm(1)</script>']
+             try:
+                 parametizer(linea,output)
+                 with open(output, "r") as f:
+                     for i in f.readlines():
+                         i = i.strip()
+                         if i == "" or i.startswith("#"):
+                             continue
+                         uri.append(i)            
+                 print()        
+                 print('\033[1;33mTest xss for default payload:\033[0m')
+                 print()        
+                 xss(uri,wordlist,vulnerables_urls)           
+             except: 
                   continue
      except:
-        continue
+         continue
      try:    
          if s:
              uri=[]
@@ -190,24 +175,23 @@ def all_list(l,c,cl,h,x,lf,s,i,sr,output,fname,o,vulnerables_urls):
          continue        
      try:    
          if lf:
-            uri=[]
-            wordlist=['../../../../../../../../../../../../../../../../../../../../../../etc/passwd']   
-            try:
-                parametizer(linea,output)
-                with open(output, "r") as f:
-                    for i in f.readlines():
-                        i = i.strip()
-                        if i == "" or i.startswith("#"):
-                            continue
-                        uri.append(i)            
-                print()        
-                print('\033[1;33mTest lfi for default payload:\033[0m')
-                print()        
-                lfi(uri,wordlist,vulnerables_urls)           
-            except: 
+             uri=[]
+             wordlist=['../../../../../../../../../../../../../../../../../../../../../../etc/passwd']   
+             try:
+                 parametizer(linea,output)
+                 with open(output, "r") as f:
+                     for i in f.readlines():
+                         i = i.strip()
+                         if i == "" or i.startswith("#"):
+                             continue
+                         uri.append(i)            
+                 print()        
+                 print('\033[1;33mTest lfi for default payload:\033[0m')
+                 print()        
+                 lfi(uri,wordlist,vulnerables_urls)           
+             except: 
                   continue
      except:
-         continue
+         continue          
      if o:
-         save_output(vulnerables_urls,fname,linea)    
-           
+         save_output(vulnerables_urls,fname,linea)  
