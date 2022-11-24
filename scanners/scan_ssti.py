01bc08ec7d79219629a50f8dbada87746ce9fc8d
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2022-11-24 16:31:55 -0300

    Create ssti scanner in new version

---
+++ scanners/scan_ssti.py
@@ -0,0 +1,100 @@
+import mechanize
+import requests
+from urllib import parse as urlparse
+import http.cookiejar
+import sys,os
+import random
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
+from colorama import Back, Fore, init
+init()
+
+
+wordlist=[
+        "daemon=",
+        "upload=",
+        "dir=",
+        "download=",
+        "log=",
+        "ip=",
+        "cli=",
+        "cmd=",
+        "exec=",
+        "command=",
+        "execute=",
+        "ping=",
+        "query=",
+        "jump=",
+        "code=",
+        "reg=",
+        "do=",
+        "func=",
+        "arg=",
+        "option=",
+        "load=",
+        "process=",
+        "step=",
+        "read=",
+        "function",
+        "req=",
+        "feature=",
+        "exe=",
+        "module=",
+        "payload=",
+        "run=",
+        "print="
+        ]
+
+user_agents = [
+                            "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8",
+                            "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre",
+                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)",
+                            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)",
+                            "Mozilla/3.01 (Macintosh; PPC)",
+                            "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",  
+                            "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",  
+                            "Opera/8.00 (Windows NT 5.1; U; en)",  
+                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
+                          ]
+user_agent = random.choice (user_agents)
+headers = {'User-Agent': user_agent}
+
+def ssti(l,wi,urls_vulnerables):
+    print()
+    print('---------------------')
+    print('\033[1;36m Testing SSTI:\033[0m') 
+    print('---------------------')
+    print()
+    limp=''
+    found=0
+    for linea in l:   
+     for li in wordlist:
+         if li in linea:
+            for line in l:
+                for w in wi:
+                     if 'FUZZ' in line:
+                         line= line.replace('=FUZZ',f'={w}')
+                         line= line.replace(' ','%20')
+                     elif '=' and not 'FUZZ' in line:
+                         line= line.replace('=',f'={w}')
+                         line= line.replace(' ','%20')                         
+                     try:
+                         req= requests.get(line,headers=headers,timeout=50)
+                         body= str(urlopen(line).read()).lower()
+                         if 'root:x' in body:
+                             found= found + 1
+                             if found == 1:
+                                 urls_vulnerables.append('\n****************** VULNERABLE TO SSTI: *********************\n')             
+                             print ('\033[1;32m[+]\033[0m ' + linea)
+                             urls_vulnerables.append(linea)  
+                     except:
+                         continue
+                     line= line.replace('%20',' ')
+                     line= line.replace(f'{w}',limp)
+         else:
+             continue 
+    if found >= 1:
+     print()   
+     print (f'\033[1;32m[+] Found [{found}] SSTI parameter/s"\033[0m')
+    else:
+     print("\033[1;31m[-] No results found\033[0m")
