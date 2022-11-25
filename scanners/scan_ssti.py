8049e80430063150f4a93fb2222a505b5d9a6d08
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2022-11-25 13:55:34 -0300

    v3

--- scanners/scan_ssti.py
+++ scanners/scan_ssti.py
@@ -98,3 +98,27 @@ def ssti(l,wi,urls_vulnerables):
      print (f'\033[1;32m[+] Found [{found}] SSTI parameter/s"\033[0m')
     else:
      print("\033[1;31m[-] No results found\033[0m")
+
+def ssti_params(l,params):
+    print()
+    print('---------------------')
+    print('\033[1;36m Testing SSTI parameters:\033[0m') 
+    print('---------------------')
+    print()
+    limp=''
+    found=0
+    for linea in l:   
+     for li in wordlist:
+         if li in linea:
+             found= found + 1
+             if found == 1:
+                 params.append('\n****************** PARAMETERS TO SSTI: *********************\n') 
+             print('\033[1;32m[+]\033[0m ' + linea)
+             params.append(linea)
+         else:
+             continue
+    if found >= 1:
+     print()
+     print (f'\033[1;32m[+] Found [{found}] SSTI parameter/s"\033[0m')
+    else:
+     print("\033[1;31m[-] No results found\033[0m")
