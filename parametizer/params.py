2d6066046e7b87ef1c4c5bde23a8f61f0fbdd9ef
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2022-12-16 09:26:30 -0300

    Update version

--- parametizer/params.py
+++ parametizer/params.py
@@ -30,7 +30,26 @@ def parametizer(url,output):
     print(f"\033[1;32m[+] Total urls found : {len(final_uris)}\033[1;31m")
 
 
+def parametizer2(url, output):
+    
+    print('\033[1;33mSearch parameters:\n\033[0m')
+    if os.name == 'nt':
+      os.system('cls')
+    url = f"https://web.archive.org/cdx/search/cdx?url=*.{url}/*&output=txt&fl=original&collapse=urlkey&page=/"    
+    retry = True
+    retries = 0
+    while retry == True and retries <= int(3):
+             response, retry = requester.connector(url)
+             retry = retry
+             retries   += 1
+    if response == False:
+         return 
+    response = unquote(response)
 
+    final_uris = extractor.param_extract2(response)
+    save_it.save_func(final_uris , output , url)
+
+    print(f"\033[1;32m[+] Total urls found : {len(final_uris)}\033[1;31m")
 
 
 
