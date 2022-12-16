6f7d5ac9eed9c5c051c6a35796b985990e3aafb7
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2022-12-16 09:27:03 -0300

    Update version

--- parametizer/core/extractor.py
+++ parametizer/core/extractor.py
@@ -18,3 +18,21 @@ def param_extract(response, holder):
         final_uris.append((i[:delim+1] + holder))
     
     return list(set(final_uris))
+
+def param_extract2(response):
+
+    ''' 
+    Function to extract URLs with parameters
+    regexp : r'.*?:\/\/.*\?.*\=[^$]'
+    
+    '''
+
+    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*\=[^$]' , response)))
+    final_uris = []
+        
+    for i in parsed:
+        #delim = i.find('=')
+        #second_delim = i.find('=', i.find('=') + 1)
+        final_uris.append(i)
+    
+    return list(set(final_uris))
