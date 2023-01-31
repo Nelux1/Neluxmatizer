951fc1ac77f3478c16db31c4f9522d6845706200
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2023-01-31 10:25:48 -0300

    Resolving problem with rce

--- scanners/scan_rce.py
+++ scanners/scan_rce.py
@@ -90,8 +90,8 @@ def rce(l,wi,urls_vulnerables):
                              found= found + 1
                              if found == 1:
                                  urls_vulnerables.append('\n****************** VULNERABLE TO RCE: *********************\n')             
-                             print ('\033[1;32m[+]\033[0m ' + linea)
-                             urls_vulnerables.append(linea)  
+                             print ('\033[1;32m[+]\033[0m ' + line)
+                             urls_vulnerables.append(line)  
                      except:
                          continue
                      line= line.replace('%20',' ')
