3f351ececde22f8a2c752a1db4d379e4784a0f88
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2023-04-03 12:18:22 -0300

    repair redirects params

--- scanners/scan_redirect.py
+++ scanners/scan_redirect.py
@@ -153,7 +153,6 @@ def redirect_params(l,params):
              found= found + 1
              if found == 1:
                  params.append('\n****************** PARAMETERS TO REDIRECT: *********************\n') 
-             print (Cursor.BACK(50) + Cursor.UP(1) + '                                 ')
              print('\033[1;32m[+]\033[0m ' + linea)
              params.append(linea)
          else:
