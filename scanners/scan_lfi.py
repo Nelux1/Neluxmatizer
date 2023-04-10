4018f6bf37426f3d94b62206195868cc4604c661
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2023-04-10 11:38:13 -0300

    update 4.0

--- scanners/scan_lfi.py
+++ scanners/scan_lfi.py
@@ -1,4 +1,4 @@
-import mechanize
+from concurrent.futures import ThreadPoolExecutor
 import requests
 from urllib import parse as urlparse
 import http.cookiejar
@@ -60,7 +60,7 @@ user_agents = [
 user_agent = random.choice (user_agents)
 headers = {'User-Agent': user_agent}
 
-def lfi(l,wi,urls_vulnerables):
+def lfi(l,wi,urls_vulnerables,threads):
     print()
     print('---------------------')
     print('\033[1;36m Testing LFI parameters:\033[0m') 
@@ -68,37 +68,46 @@ def lfi(l,wi,urls_vulnerables):
     print()
     limp=''
     found=0
-    for linea in l:   
-     for li in wordlist:
-         if li in linea:
-            for line in l:
-                for w in wi:
-                     if len(urls_vulnerables) == 0:
-                         print(Cursor.BACK(50) + Cursor.UP(0) + "\033[46m-_-_-_-_- TESTING -_-_-_-_-\033[0m")
-                         sleep(1)
-                         print(Cursor.BACK(50) + Cursor.UP(1) + "\033[1;36m_-_-_-_-_   WAIT  _-_-_-_-_\033[0m")
-                     if 'FUZZ' in line:
-                         line= line.replace('=FUZZ',f'={w}')
-                         line= line.replace(' ','%20')
-                     elif '=' and not 'FUZZ' in line:
-                         line= line.replace('=',f'={w}')
-                         line= line.replace(' ','%20')                         
-                     try:
-                         req= requests.get(line,headers=headers,timeout=50)
-                         body= str(urlopen(line).read()).lower()
-                         if 'root:x' in body:
-                             found= found + 1
-                             if found == 1:
-                                 urls_vulnerables.append('\n****************** VULNERABLE TO LFI: *********************\n')             
-                                 print (Cursor.BACK(50) + Cursor.UP(1) + '                                 ')
-                             print ('\033[1;32m[+]\033[0m ' + linea)
-                             urls_vulnerables.append(linea)  
-                     except:
-                         continue
-                     line= line.replace('%20',' ')
-                     line= line.replace(f'{w}',limp)
-         else:
-             continue 
+
+    def lfi_single(line,w):
+     nonlocal found
+
+     if len(urls_vulnerables) == 0:
+         print(Cursor.BACK(50) + Cursor.UP(0) + "\033[46m-_-_-_-_- TESTING -_-_-_-_-\033[0m")
+         sleep(1)
+         print(Cursor.BACK(50) + Cursor.UP(1) + "\033[1;36m_-_-_-_-_   WAIT  _-_-_-_-_\033[0m")
+
+     if 'FUZZ' in line:
+         line= line.replace('=FUZZ',f'={w}')
+         line= line.replace(' ','%20')
+     elif '=' and not 'FUZZ' in line:
+         line= line.replace('=',f'={w}')
+         line= line.replace(' ','%20')  
+                                
+     try:
+          req= requests.get(line,headers=headers,timeout=50)
+          body= str(urlopen(line).read()).lower()
+          if 'root:x' in body:
+              found= found + 1
+              if found == 1:
+                 urls_vulnerables.append('\n****************** VULNERABLE TO LFI: *********************\n')             
+                 print (Cursor.BACK(50) + Cursor.UP(1) + '                                 ')
+              print ('\033[1;32m[+]\033[0m ' + linea)
+              urls_vulnerables.append(linea)  
+     except:
+         pass
+     line= line.replace('%20',' ')
+     line= line.replace(f'{w}',limp)
+
+    
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+     for linea in l:   
+         for li in wordlist:
+             if li in linea:
+                 for line in l:
+                     for w in wi:
+                         executor.submit(lfi_single,line,w)   
+
     if found >= 1:
      print()   
      print (f'\033[1;32m[+] Found [{found}] LFI parameter/s"\033[0m')
@@ -108,25 +117,29 @@ def lfi(l,wi,urls_vulnerables):
      print("\033[1;31m[-] No results found\033[0m")
      print()
 
-def lfi_params(l,params):
+def lfi_params(l,params,threads):
     print()
     print('---------------------')
     print('\033[1;36m Testing LFI parameters:\033[0m') 
     print('---------------------')
     print()
-    limp=''
     found=0
-    for linea in l:   
-     for li in wordlist:
+    
+    def lfip_single(linea,li):
+         nonlocal found
          if li in linea:
              found= found + 1
              if found == 1:
                  params.append('\n****************** PARAMETERS TO LFI: *********************\n')
-             print (Cursor.BACK(50) + Cursor.UP(1) + '                                 ')   
              print('\033[1;32m[+]\033[0m ' + linea)
              params.append(linea)
-         else:
-             continue
+
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+     for linea in l:   
+         for li in wordlist:
+             executor.submit(lfip_single,linea,li)   
+
+
     if found >= 1:
      print()
      print (f'\033[1;32m[+] Found [{found}] LFI parameter/s"\033[0m')
