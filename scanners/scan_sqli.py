172b6a54293c6b6e7d85161e6f3946118c4a2654
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2023-04-11 18:47:04 -0300

    update 4.0.3

--- scanners/scan_sqli.py
+++ scanners/scan_sqli.py
@@ -80,10 +80,6 @@ def sqli(l,wi,urls_vulnerables,threads):
     def sql_single(linea,w):
       nonlocal found
 
-      if found == 0:
-         print(Cursor.BACK(50) + Cursor.UP(0) + "\033[46m-_-_-_-_- TESTING -_-_-_-_-\033[0m")
-         sleep(1)
-         print(Cursor.BACK(50) + Cursor.UP(1) + "\033[1;36m_-_-_-_-_   WAIT  _-_-_-_-_\033[0m")      
       if 'FUZZ' in linea:
          linea= linea.replace('=FUZZ',f'={w}')
       elif '=' and not 'FUZZ' in linea:
@@ -94,11 +90,11 @@ def sqli(l,wi,urls_vulnerables,threads):
          for x in responses:
              if x in req.text:  
                  found= found + 1
-             if found == 1:
-                 urls_vulnerables.append('\n****************** PARAMETERS TO SQL: *********************\n')
-                 print (Cursor.BACK(50) + Cursor.UP(1) + '                                 ')
-             print('\033[1;32m[+]\033[0m ' + linea)
-             urls_vulnerables.append(linea)
+                 if found == 1:
+                     urls_vulnerables.append('\n****************** PARAMETERS TO SQL: *********************\n')
+                     print (Cursor.BACK(50) + Cursor.UP(1) + '                                 ')
+                 print('\033[1;32m[+]\033[0m ' + linea)
+                 urls_vulnerables.append(linea)
       except:
          pass
       linea= linea.replace(f'={w}','=FUZZ')
