2ef74111ba759b53f6934698e26fcd5913a7c72f
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2023-05-26 19:52:58 -0300

    Update scan_idor.py 5.3

--- scanners/scan_idor.py
+++ scanners/scan_idor.py
@@ -1,8 +1,5 @@
 import requests
 from urllib import parse as urlparse
-import http.cookiejar
-import sys,os
-import random
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 from colorama import Back, Fore, Cursor, init
@@ -22,25 +19,12 @@ wordlist=[
  "group=",
  "profile=",
  "edit=",
- "report="
+ "report=",
+ "id="
 ]
 
 
-user_agents = [
-                            "Mozilla/5.0 (X11; U; Linux i686; it-IT; rv:1.9.0.2) Gecko/2008092313 Ubuntu/9.25 (jaunty) Firefox/3.8",
-                            "Mozilla/5.0 (X11; Linux i686; rv:2.0b3pre) Gecko/20100731 Firefox/4.0b3pre",
-                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)",
-                            "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; en)",
-                            "Mozilla/3.01 (Macintosh; PPC)",
-                            "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.9)",  
-                            "Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01",  
-                            "Opera/8.00 (Windows NT 5.1; U; en)",  
-                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.19 (KHTML, like Gecko) Chrome/0.2.153.1 Safari/525.19"
-                          ]
-user_agent = random.choice (user_agents)
-headers = {'User-Agent': user_agent}
-
-def idor(l,w,urls_vulnerables,params,threads):
+def idor(l,params,threads):
     print()
     print('---------------------')
     print('\033[1;36m Testing IDOR parameters:\033[0m') 
@@ -68,3 +52,4 @@ def idor(l,w,urls_vulnerables,params,threads):
     else:
      print("\033[1;31m[-] No results found\033[0m")
      print()
+
