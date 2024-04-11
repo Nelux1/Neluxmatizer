33111cbb733f7b668260a6e83cc64583ef573749
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2024-04-11 16:01:44 -0300

    Update scan cors get-post methods

--- scanners/scan_cors.py
+++ scanners/scan_cors.py
@@ -24,11 +24,15 @@ def cors(l,vulnerables_urls,threads):
         nonlocal p,f
         try:
             req=requests.get(linea, headers=headers, timeout=10.0) #Opens the url
+            if req.status_code == 405 or req.status_code == 501:
+                   req=requests.post(linea,headers=headers, timeout=10.0)
         except URLError as e:
             for line in linea:
                 linea = line.replace('http://', 'https://')
             try:    
                 req=requests.get(linea,timeout=10.0)
+                if req.status_code == 405 or req.status_code == 501:
+                   req=requests.post(linea,timeout=10.0)
             except URLError:        
                 print(f'\033[1;31{req.status_code} [?] open Url Error\033[0m')
                 return
@@ -37,13 +41,13 @@ def cors(l,vulnerables_urls,threads):
              update_progress(p,total)             
              pass                                        
         try:
-            if 'access-control-allow-origin' in req.headers:
-                if 'https://evil.com' in req.headers:
+            if 'access-control-allow-origin' in req.headers and 'https://evil.com' in req.headers:
                     f+=1
                     print ('\033[1;32m[+]\033[0m ' + req.url)
+                    print (req.headers)
                     print("\x1b[1;35m[*]\033[0;m"+f" Access-Control-Allow-Origin: {req.headers['Access-Control-Allow-Origin']}")
                     if f == 1:
-                     vulnerables_urls.append('\n****************** VULNERABLE TO CORS: *********************\n') 
+                     vulnerables_urls.append('\n******* VULNERABLE TO CORS: ********\n') 
                     vulnerables_urls.append(linea)
             p+=1
             update_progress(p,total)                                   
@@ -62,4 +66,4 @@ def cors(l,vulnerables_urls,threads):
  else:
      print (Cursor.BACK(50) + Cursor.UP(1) + '      '*80)        
      print(Cursor.BACK(50) + Cursor.UP(1) + "\033[1;31m[-] No results found\033[0m")
-     print()  
+     print()
