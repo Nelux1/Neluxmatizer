ecbac96bcf28d151968a846910f6e150d15969fb
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2025-04-12 15:29:10 -0300

    Update extractor.py

--- parametizer/core/extractor.py
+++ parametizer/core/extractor.py
@@ -1,38 +1,34 @@
-import re 
-
+import re
 
 def param_extract(response, holder):
-
-    ''' 
-    Function to extract URLs with parameters
-    regexp : r'.*?:\/\/.*\?.*\=[^$]'
     
+    r''' 
+    Function to extract URLs with parameters and replace values with a holder string.
+    r'.*?:\/\/.*\?.*=.*'
     '''
 
-    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*\=[^$]' , response)))
+    
+    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*=.*', response)))
     final_uris = []
         
     for i in parsed:
         delim = i.find('=')
-        second_delim = i.find('=', i.find('=') + 1)
-        final_uris.append((i[:delim+1] + holder))
+        final_uris.append(i[:delim + 1] + holder)
     
     return list(set(final_uris))
 
 def param_extract2(response):
-
-    ''' 
-    Function to extract URLs with parameters
-    regexp : r'.*?:\/\/.*\?.*\=[^$]'
     
+    r''' 
+    Function to extract full URLs with parameters without modification.
+    r'.*?:\/\/.*\?.*=.*'
     '''
 
-    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*\=[^$]' , response)))
+    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*=.*', response)))
     final_uris = []
-        
+
     for i in parsed:
-        #delim = i.find('=')
-        #second_delim = i.find('=', i.find('=') + 1)
         final_uris.append(i)
     
     return list(set(final_uris))
+
